cba339bcf6399a1800c31e70c6f44178b99e66d6
torymindol3-boop <torymindol3@gmail.com>
2025-09-05 18:37:14 +0900

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main style={{ minHeight: "100vh", display: "grid", placeItems: "center", color: "white" }}>
+      <div>It works 🎉</div>
+    </main>
+  )
+}
